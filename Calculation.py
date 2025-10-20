8585ed28803142521c4786a8139eac92a8442e40
mishrp13 <prabalsonu1997@gmail.com>
2025-10-20 17:46:52 +0530

    new changes

---
+++ Calculation.py
@@ -0,0 +1,7 @@
+print(f"20 days are {20*24*60} minutes")
+print(f"50 days are {50*24*60} minutes")
+
+
+calcualtion_to_seconds=24*60*60
+name_of_unit="seconds"
+print(f"34 days are {34*calcualtion_to_seconds} {name_of_unit}")
