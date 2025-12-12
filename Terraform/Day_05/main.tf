8370125d2f0e44f7589ed84879c8f80cfbe39546
mishrp13 <prabalsonu1997@gmail.com>
2025-12-12 18:05:53 +0530

    new changes

---
+++ Terraform/Day_05/main.tf
@@ -0,0 +1,83 @@
+
+
+variable "environment" {
+
+    default = "dev"
+    type = string
+  
+}
+
+variable "channel_name" {
+
+    default = "ttwp"
+  
+}
+
+variable "region" {
+
+    default = "us-east-1"
+  
+}
+
+locals {
+    
+   bucket_name= "${var.channel_name}-${var.environment}-123-${var.region}"
+   vpc_name = "${var.environment}-VPC"
+}
+
+
+resource "aws_s3_bucket" "first_bucket" {
+
+    bucket=local.bucket_name
+
+    tags ={
+        Name = local.bucket_name
+        Environment = var.environment
+    }
+  
+}
+
+resource "aws_vpc" "sample" {
+
+    cidr_block = "10.0.1.0/24"
+    region = var.region
+    tags= {
+
+        Environment = var.environment
+        Name = local.vpc_name
+
+    }
+  
+}
+
+resource "aws_instance" "example" {
+    ami= "resolve:ssm:/aws/service/ami-amazon-linux-latest/al2023-ami-kernel-default-x86_64"
+    instance_type = "t2.micro"
+    region = var.region
+
+    tags = {
+        Environment = var.environment
+        Name = "${var.environment}-EC2-Instance"
+    }
+  
+}
+output "vpc_id" {
+
+    value = aws_vpc.sample.id
+  
+}
+
+output "ec2_id" {
+
+    value = aws_instance.example.id
+  
+}
+
+#export TF_VAR_environment=stage
+
+# precedence
+# -var=environment=prod > terraform.tfvars > Export (Env Variable) > default
+
+
+
+
