924a51e7cf606a7825fcd4d61235b52a43d83a4a
mishrp13 <prabalsonu1997@gmail.com>
2025-12-26 17:31:22 +0530

    new changes

--- Terraform/Day_13/main.tf
+++ Terraform/Day_13/main.tf
@@ -1,50 +1,44 @@
 
 data "aws_vpc" "vpc_name"{
   filter {
-    name= "tag:name"
+    name= "tag:Name"
     values= ["default"]
   }
 }
 
-data aws_subnet "shared" {
+data "aws_subnet" "shared" {
 
   filter {
 
-    name= "tag:name"
+    name= "tag:Name"
     values = ["subneta"]
 
   }
 
   vpc_id = data.aws_vpc.vpc_name.id
 }
-
-data "aws_ami" "linux2"{
-
-  owners = ["amazon"]
+data "aws_ami" "linux2" {
+  owners      = ["amazon"]
   most_recent = true
 
   filter {
-    
-    name= "name"
-    values = ["amzn2-ami-hvm-*-86_4-gp2"]
-
+    name   = "name"
+    values = ["amzn2-ami-hvm-*-x86_64-gp2"]
   }
-  
+
   filter {
-
-     name= "virtualization-type"
-     values = ["hvm"]
-
+    name   = "virtualization-type"
+    values = ["hvm"]
   }
-
 }
+
 
 
 resource "aws_instance" "example" {
   ami  = data.aws_ami.linux2.id
   
   instance_type = "t2.micro"
-  subnet_id= data.aws_subnet.shared
+  subnet_id= data.aws_subnet.shared.id
   tags = var.tags
 }
 
