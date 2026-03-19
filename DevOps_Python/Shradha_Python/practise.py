fef7ed1beeee07d0977bc568a074a8ea7e9f4d92
mishrp13 <prabalsonu1997@gmail.com>
2026-03-19 12:58:49 +0530

    new changes

--- DevOps_Python/Shradha_Python/practise.py
+++ DevOps_Python/Shradha_Python/practise.py
@@ -1,22 +1,4 @@
-def is_phone_number(text):
-    if len(text) != 12:
-        return False
-    for i in range(0,3):
-        if not text[i].isdecimal():
-            return False
-    if text[3] != '-':
-        return False
-    for i in range(4,7):
-        if not text[i].isdecimal():
-            return False
-    if text[7] != '-':
-        return False
-    for i in range(8,12):
-        if not text[i].isdecimal():
-            return False
-    return True
-
-print('Is 415-555-4242 a phone number?', is_phone_number('415-555-4242'))
-print('Is moso moso a phone number ?', is_phone_number('moso moso'))
-
-
+import re
+phone_number_pattern_object=re.compile(r'\d{3}-\d{3}-\d{4}')
+match_obj=phone_number_pattern_object.search("my number is 123-456-0974")
+print(match_obj.group())
