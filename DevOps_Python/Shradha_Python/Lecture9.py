d3b1baa1c9469fa51a498d76b76951a3d3f555a0
mishrp13 <prabalsonu1997@gmail.com>
2026-03-22 11:39:52 +0530

    new changes

--- DevOps_Python/Shradha_Python/Lecture9.py
+++ DevOps_Python/Shradha_Python/Lecture9.py
@@ -247,31 +247,104 @@
 # operator overloading
 
 
-class Complex:
+# class Complex:
     
-    def __init__(self,real,img):
-        self.real=real
-        self.img=img
+#     def __init__(self,real,img):
+#         self.real=real
+#         self.img=img
     
     
      
-    def showNumber(self):
-        print(self.real,"i +", self.img, "j")
+#     def showNumber(self):
+#         print(self.real,"i +", self.img, "j")
     
-              
+#     def __add__(self,num8):  # dunder function
+#         newReal= self.real + num8.real
+#         newImg= self.img + num8.img
+#         return Complex(newReal,newImg)
+    
+#     def __sub__(self,num8):  # dunder function
+#         newReal= self.real - num8.real
+#         newImg= self.img - num8.img
+#         return Complex(newReal,newImg)
+    
+
+# num1 = Complex(1,3)
+# num1.showNumber()
 
 
+# num2 = Complex(4,6)
+# num2.showNumber()
 
 
-num1 = Complex(1,3)
-num1.showNumber()
+# num3= num1 + num2
+# num3.showNumber()
 
 
-num2 = Complex(4,6)
-num2.showNumber()
+# num3= num1 - num2
+# num3.showNumber()
 
-# start from 50 min
+# # num3 = num1.add(num2)
+# # num3.showNumber()
 
+#---------------------------------------------------
+
+# class Circle:
+
+#     def __init__(self,radius):
+#         self.radius=radius
+
+#     def area(self):
+#         return (22/7) * self.radius**2
+    
+#     def perimeter(self):
+#         return 2 * (22/7) * self.radius
+    
+
+# c1 = Circle(21)
+# print(c1.area())
+# print(c1.perimeter())
+
+#------------------------------------------------------
+
+# class Employee:
+
+#     def __init__(self,role,dept,salary):
+#         self.role=role
+#         self.dept=dept
+#         self.salary=salary
+
+#     def showDetails(self):
+#         print("role =",self.role)
+#         print("dept =",self.dept)
+#         print("salary =",self.salary)
+
+
+# class Engineer(Employee):
+#     def __init__(self,name,age):
+#         self.name=name
+#         self.age=age
+#         super().__init__("Engineer","IT","75,000")
+
+
+# engg1= Engineer("Elon musk",40)
+# engg1.showDetails()
+
+
+#-----------------------------------------------
+
+class Order:
+
+    def __init__(self,item,price):
+        self.item=item
+        self.price=price
+
+    def __gt__(self,ord2):
+        return self.price > ord2.price
+    
+ord1= Order("chips",20)
+ord2= Order("Mango",56)
+print(ord1 > ord2)
 
 
 
