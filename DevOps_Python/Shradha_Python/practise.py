c1cced86b5c83aa782a113befde359ea379e21f5
mishrp13 <prabalsonu1997@gmail.com>
2026-04-08 11:55:58 +0530

    new changes

--- DevOps_Python/Shradha_Python/practise.py
+++ DevOps_Python/Shradha_Python/practise.py
@@ -1,30 +1,21 @@
 class Solution:
 
-    def second_largest(self,nums):
-
-        n =len(nums)
-
-        if n <2:
-            return -1
-        
-        largest= float('-inf')
-        self.second_largest=float('-inf')
-
+    def Pattern(self,n):
         for i in range(n):
-            largest= max(largest, nums[i])
-
-        for i in range(n):
-            if nums[i] > self.second_largest and nums[i] != largest:
-                self.second_largest=nums[i]
-
-        return -1 if self.second_largest == float('-inf') else self.second_largest
-    
-
-nums = [1,2,3,4,56]
-sol =Solution()
-ans= sol.second_largest(nums)
-print("second largest", ans)
+            for j in range(n):
+                print("*", end= "")
+            print()
 
 
+    def main(self):
+        sol=Solution()
+        N=4
+        sol.Pattern(N)
 
-       
+
+if __name__=="__main__":
+    Solution().main()
+
+
+
+
